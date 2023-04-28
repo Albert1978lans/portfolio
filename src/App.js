1a9c70132de9f238b49265812fbb70bd6c9ca37e
Albert <bert21387@gmail.com>
2023-04-28 11:52:09 +0500

    Header Main

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./Header/Header";
+import Main from "./main/Main";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Main/>
+        </div>
+    );
 }
 
 export default App;
